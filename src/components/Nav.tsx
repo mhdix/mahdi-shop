12b9ab000364cde49d2e7fcd04e26d033f55ac27
mhdix <domanloom@gmail.com>
2024-11-17 03:21:51 -0800

    feature: create react-router-dom and create navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
+import Container from "./container/container";
 
 const Nav = () => {
   return (
-    <div className="h-14 border-b shadow flex justify-between flex-row-reverse items-center container mx-auto">
-      <ul className="flex">
-        <li>
-          <Link to="/">خانه</Link>
-        </li>
-        <li>
-          <Link to="/store">فروشگاه</Link>
-        </li>
-      </ul>
-      <div>سبد خرید</div>
+    <div className="h-14 border-b shadow flex items-center">
+      <Container>
+        <div className=" flex justify-between flex-row-reverse items-center">
+          <ul className="flex flex-row-reverse">
+            <li className="ml-4">
+              <Link to="/">خانه</Link>
+            </li>
+            <li className="ml-4">
+              <Link to="/store">فروشگاه</Link>
+            </li>
+          </ul>
+          <div>سبد خرید</div>
+        </div>
+      </Container>
     </div>
   );
 };
